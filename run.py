1781c3f9bb4eb01cf5266b0c7a3de4599b00d8a3
Ga1axy_Win <2864630476@qq.com>
2025-11-10 13:33:30 +0800

    feat:增加python运行脚本

---
+++ run.py
@@ -0,0 +1,12 @@
+import streamlit.web.cli as stcli
+import sys,os
+
+if __name__ == "__main__":
+    if getattr(sys,'frozen',False):
+        current_dir = sys._MEIPASS
+    else:
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, "gy.py")
+
+    sys.argv = ["streamlit", "run", file_path]
+    sys.exit(stcli.main())
